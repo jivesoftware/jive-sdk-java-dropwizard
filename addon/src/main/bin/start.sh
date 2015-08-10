a467ad0103ac18666637caf3e47a0563b6b5c7e6
Chris Dempsey <chris.dempsey@jivesoftware.com>
2015-08-10 11:45:48 -0700

    Assembly process hooked in

---
+++ addon/src/main/bin/start.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+java -jar ./jive-example-1.0.0-SNAPSHOT-shaded.jar server addon.yml
---
+++ addon/src/main/java/com/jivesoftware/ps/customer/JiveAddOn.java
@@ -0,0 +1,48 @@
+package com.jivesoftware.ps.customer;
+
+import com.jivesoftware.ps.addons.AbstractApplication;
+import io.dropwizard.Configuration;
+import io.dropwizard.assets.AssetsBundle;
+import io.dropwizard.setup.Bootstrap;
+import io.dropwizard.setup.Environment;
+
+/**
+ *
+ *
+ * @see AbstractApplication
+ * @see Configuration
+ */
+public class JiveAddOn extends AbstractApplication<Configuration> {
+
+    /**
+     * Returns the name of the application.
+     *
+     * @return the application's name
+     */
+    @Override
+    public String getName() {
+        return "Jive Example Add-On";
+    }
+
+    /**
+     * @param bootstrap pre-start application environment
+     */
+    @Override
+    public void onInitialize(Bootstrap<Configuration> bootstrap) {
+        bootstrap.addBundle(new AssetsBundle("/assets", "/", "index.html"));
+    }
+
+    /**
+     * @param configuration the parsed {@link Configuration} object
+     * @param environment   the application's {@link Environment}
+     * @throws Exception if something goes wrong
+     */
+    @Override
+    public void onRun(Configuration configuration, Environment environment) throws Exception {
+        environment.jersey().setUrlPattern("/api/*");
+    }
+
+    public static void main(String[] args) throws Exception {
+        new JiveAddOn().run(args);
+    }
+}
